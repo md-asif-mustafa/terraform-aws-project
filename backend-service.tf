dcba549cd9d4e4a1bf4b664e40c98b2a4d175309
md-asif-mustafa <mdasif.april@gmail.com>
2024-06-15 15:18:14 +0530

    updated backend service version

--- backend-service.tf
+++ backend-service.tf
@@ -16,9 +16,9 @@ resource "aws_db_instance" "aprofile-rds" {
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
-  engine_version         = "5.6.34"
+  engine_version         = "8.0.36"
   instance_class         = "db.t2.micro"
-  db_name                   = var.dbname
+  db_name                = var.dbname
   username               = var.dbuser
   password               = var.dbpass
   parameter_group_name   = "default.mysql5.6"
@@ -43,7 +43,7 @@ resource "aws_elasticache_cluster" "aprofile-cache" {
 resource "aws_mq_broker" "aprofile-rmq" {
   broker_name        = "aprofile-rmq"
   engine_type        = "ActiveMQ"
-  engine_version     = "5.15.0"
+  engine_version     = "5.17.6"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.aprofile-backend-sg.id]
   subnet_ids         = [module.vpc.private_subnets[0]]
